372283831d920e8d1dd3b843d2647b05b0f7b9ee
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2025-08-13 14:27:52 +0530

    Created the Album Page
    Built a single layout for different albums displayed.

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -6,21 +6,37 @@ interface MusicStore{
   albums: Song[],
   songs: Album[],
   isLoading: boolean,
-  error: string | null;
+  error: string | null,
+  currentAlbum: Album | null,
 
   fetchAlbums: ()=> Promise<void>;
+  fetchAlbumById: (id: string) => Promise<void>;
 }
+
 export const useMusicStore = create<MusicStore>((set) => ({
   albums: [],
   songs: [],
   isLoading: false,
   error: null,
+  currentAlbum: null,
 
   fetchAlbums: async () =>{
     set({isLoading: true, error:null});
     try {
       const response = await axiosInstance.get("/albums")
       set({albums:response.data})
+    } catch (error: any) {
+      set({ error: error.response.data.message});
+    } finally{
+      set({isLoading: false});
+    }
+  },
+
+  fetchAlbumById: async (id) =>{
+    set({isLoading: true, error:null});
+    try {
+      const response = await axiosInstance.get(`/albums/${id}`)
+      set({currentAlbum : response.data})
     } catch (error: any) {
       set({ error: error.response.data.message});
     } finally{
