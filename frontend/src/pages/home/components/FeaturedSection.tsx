990f0b0926e3cb7976e5e3ec352ec6b6d7e057f2
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2025-08-13 21:28:46 +0530

    Play Button Component implemented
    Created the Play Button and gave it a prop as the respective current song.

--- frontend/src/pages/home/components/FeaturedSection.tsx
+++ frontend/src/pages/home/components/FeaturedSection.tsx
@@ -26,7 +26,7 @@ const FeaturedSection = () => {
 						<p className='font-medium truncate'>{song.title}</p>
 						<p className='text-sm text-zinc-400 truncate'>{song.artist}</p>
 					</div>
-					<PlayButton/>
+					<PlayButton song={song}/>
 				</div>
 			))}
 		</div>
