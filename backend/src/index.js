36374b761c74e7240a6807fb0d132938d7f8d968
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2025-08-14 20:44:56 +0530

    Added production build setup and temp file cleanup
    a. Added node-cron to backend for hourly cleanup of temporary files.
    b. Updated backend to serve frontend static files in production.
    c. Improved axios baseURL handling for frontend environments.
    d. Introduced root package.json with build/start scripts for easier deployment.

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import fileUpload from 'express-fileupload'
 import path from 'path'
 import cors from 'cors'
 import { createServer } from 'http'
+import cron from "node-cron"
+import fs from "fs"
 
 import { connectDB } from './lib/db.js'
 import { initializeSocket } from './lib/socket.js'
@@ -41,12 +43,35 @@ app.use(fileUpload({
   limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB
 }))
 
+// cron jobs
+const tempDir = path.join(process.cwd(), "tmp");
+cron.schedule("0 * * * *", () => {
+	if (fs.existsSync(tempDir)) {
+		fs.readdir(tempDir, (err, files) => {
+			if (err) {
+				console.log("error", err);
+				return;
+			}
+			for (const file of files) {
+				fs.unlink(path.join(tempDir, file), (err) => {});
+			}
+		});
+	}
+});
+
 app.use('/api/users',userRoutes)
 app.use('/api/admin',adminRoutes)
 app.use('/api/songs',songRoutes)
 app.use('/api/albums',albumRoutes)
 app.use('/api/stats',statRoutes)
 app.use('/api/auth',authRoutes)
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+	});
+}
 
 app.use((err, req, res, next) => {
   res.status(500).json({message:process.env.NODE_ENV === 'production' ? 'Internal Server Error': err.message})
